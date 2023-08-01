19f789746d13509f1d9cf287307d21e81b625db3
Logan Milkman <loganescobarc@gmail.com>
2023-07-31 23:36:39 -0600

    This is harder than i thought it would be

--- game.py
+++ game.py
@@ -1,85 +1,87 @@
 import random, time
 
-def print_wbw(text, delay=0.3):
+# Tried to replicate the way the text appears in RPGs with this, so it's not printed instantly
+def print_wbw(text, delay=0.2):
     words = text.split()
     for word in words:
         print(word, end=' ', flush=True)
         time.sleep(delay)
     print()
 
-food_weapons = {
-    "Mexico": {
-        "Taco Sword": 35,
-        "Enchilada Shield": 20,
-        "Guacamole Grenade": 45,
-        "Salsa Spear": 30,
-    },
-    "Japan": {
-        "Sushi Katana": 40,
-        "Ramen Noodles Nunchaku": 25,
-        "Wasabi Shuriken": 35,
-        "Tempura Bow": 30,
-    },
-    "Italy": {
-        "Pizza Cutter Dagger": 30,
-        "Spaghetti Lasso": 25,
-        "Gelato Boomerang": 35,
-        "Risotto Rapier": 40,
-    },
-    "India": {
-        "Curry Katar": 35,
-        "Naan Shield": 20,
-        "Samosa Bomb": 45,
-        "Chutney Chakram": 30,
-    }
-}
-
-class Character:
-    def __init__(self, name, health, items):
-        self.name = name
-        self.health = health
-        self.items = items
-
-    def take_damage(self, damage):
-        self.health -= damage
-
-    def is_alive(self):
-        return self.health > 0
-
-def attack(attacker, defender):
-    damage = random.randint(attacker.attack // 2, attacker.attack)
-    defender.take_damage(damage)
-    print_wbw(f"{attacker.name} attacks {defender.name} and deals {damage} damage.")
+# Classes for the game:
 
 class Enemy:
     def __init__(self, name, health, attack):
         self.name = name
         self.health = health
         self.attack = attack
+        self.alive = True
 
     def take_damage(self, damage):
         self.health -= damage
 
-    def is_alive(self):
-        return self.health > 0
+class Player:
+    def __init__(self, name, health, items, weapon):
+        self.name = name
+        self.health = health
+        self.items = items
+        self.weapon = weapon
+        self.alive = True
 
+    def take_damage(self, damage):
+        self.health -= damage
 
-print_wbw("Welcome to culinary battles!")
+    def attack(self, damage):
+        print_wbw(f"You attack using {self.weapon}!")
+        self.enemy.take_damage(damage)
+        print_wbw(f"You have dealt {damage} hit points!")
 
-player_name = input("What is your name?: ")
-player_health = int(input("What is your age?: ")) * 4
+    def select_and_use_item(self):
+        print_wbw(f"{self.name}, choose an item to use for healing:")
+        for index, (item, hp) in enumerate(self.items, 1):
+            print(f"{index}: {item} - {hp} HP")
 
-while True:
-    player_items = input(f"From what country do you prefer the food \n {[value for value in food_weapons.keys()]}?:\n").capitalize()
-    if player_items in food_weapons.keys():
-        break
-    else:
-        print("Hmmmm are you sure? Try to pick one of the options again please")
+        while True:
+            try:
+                choice = int(input("Enter the number of the item to select it: "))
+                if 1 <= choice <= len(self.items):
+                    selected_item = list(self.items.keys())[choice - 1]
+                    heal_amount = self.items[selected_item]
+                    self.health = min(self.health + heal_amount, 100)
+                    print(f"{self.name} used {selected_item} and recovered {heal_amount} HP.")
+                    break
+                else:
+                    print("That item doesn't exist, try again please!")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
 
-player_items = food_weapons[player_items]
+    def run_away(self):
+        funny_phrases = [
+            "You tried to run, but you tripped over your own feet. Not your finest moment.",
+            "The ground suddenly became too interesting for you to run away.",
+            "You had second thoughts about running and decided to stay for the snacks.",
+            "You pondered the meaning of life and forgot about running for a moment.",
+            "The enemy looked so adorable that you couldn't bear to leave.",
+        ]
+        print_wbw(random.choice(funny_phrases))
 
-player = Character(player_name, player_health, player_items)
-print(f"Alright! We're ready to go, just to confirm, here are the details from your profile:\nName: {player_name}\nHealth: {player_health}\nItems: {player_items}\nThe items have the name of the item and also the dmg they give")
+    def actions(self):
+        self.attack = 0
+        self.defense = 0
+        self.item = []
+        self.option = ["1. Attack", "2. Special Item", "3. Run"]
+        print_wbw("Actions:\n")
+        for option in self.option:
+            print(option)
+        selection = input("What will you do?:\n")
+        if selection == "Attack" or selection == "1":
+            Player.attack(self)
+        elif selection == "Special Item" or selection == "2":
+            Player.select_and_use_item(self)
+        elif selection == "Run" or selection == "3":
+            Player.run_away(self)
+
+# General items and behaviour of the game:
 
 enemies = [
     Enemy("Gooey Glop", health=80, attack=15),
@@ -92,17 +94,113 @@ def select_enemy():
     for index, enemy in enumerate(enemies, 1):
         print(f"{index}. {enemy.name}")
 
-    while True:
-        try:
-            enemy_choice = int(input("Enter the number of your choice: "))
-            if 1 <= enemy_choice <= len(enemies):
-                return enemies[enemy_choice - 1]
-            else:
-                print("That enemy doesn't exist, pick again!")
-        except ValueError:
-            print("Invalid input. Please enter a number.")
+food_items = {
+    "Mexico": {
+        "Great Taco": 35,
+        "Enchilada Pack": 20,
+        "Guacamole Potion": 45,
+        "Salsa Amulet": 30,
+    },
+    "Japan": {
+        "Sushi Roll": 25,
+        "Ramen Noodles": 15,
+        "Wasabi Potion": 20,
+        "Tempura Memento": 18,
+    },
+    "Italy": {
+        "Pizza Slice": 18,
+        "Spaghetti Bowl": 12,
+        "Gelato Scoop": 22,
+        "Mega Risotto": 28,
+    },
+    "India": {
+        "Curry Plate": 22,
+        "Naan Bread": 10,
+        "Samosa Chakra": 25,
+        "Chutney Dip": 16,
+    }
+}
 
-print("You encounter some enemies! Here are the enemies:")
-print()
-print_wbw(f"{', '.join(enemy.name for enemy in enemies)}")
-select_enemy()
+food_weapons = {
+    "Pizza Cutter Dagger": 35,
+    "Sushi Katana": 40,
+    "Taco Sword": 30,
+    "Baguette Staff": 25,
+    "Chili Pepper Bow": 28,
+}
+
+
+
+
+print_wbw("Welcome to culinary battles!")
+
+player_name = input("What is your name?: ")
+player_health = int(input("What is your age?: ")) * 4
+
+while True:
+    player_items = input(f"From what country do you prefer the food \n {[value for value in food_items.keys()]}?:\n").capitalize()
+    if player_items in food_items.keys():
+        break
+    else:
+        print("Hmmmm are you sure? Try to pick one of the options again please")
+
+player_items = food_items[player_items]
+
+print_wbw("It's dangerous to go alone, pick a weapon!")
+
+while True:
+    player_weapon = int(input(f"What weapon would you like to use? \n {[value for value in enumerate(food_weapons.keys(), 1)]}?:\n"))
+    if player_weapon == 1:
+        player_weapon = "Pizza Cutter Dagger"
+        print_wbw(f"You selected the {player_weapon}! it has {food_weapons[player_weapon]} hit points!")
+        break
+    elif player_weapon == 2:
+        player_weapon = "Sushi Katana"
+        print_wbw(f"You selected the {player_weapon}! it has {food_weapons[player_weapon]} hit points!")
+        break
+    elif player_weapon == 3:
+        player_weapon = "Taco Sword"
+        print_wbw(f"You selected the {player_weapon}! it has {food_weapons[player_weapon]} hit points!")
+        break
+    elif player_weapon == 4:
+        player_weapon = "Baguette Staff"
+        print_wbw(f"You selected the {player_weapon}! it has {food_weapons[player_weapon]} hit points!")
+        break
+    elif player_weapon == 5:
+        player_weapon = "Chili Pepper Bow"
+        print_wbw(f"You selected the {player_weapon}! it has {food_weapons[player_weapon]} hit points!")
+        break
+    else:
+        print("Invalid selection, please use the correct number!")
+
+player = Player(player_name, player_health, player_items, player_weapon)
+print(f"Alright! We're ready to go, just to confirm, here are the details from your profile:\nName: {player_name}\nHealth: {player_health}\nItems: {player_items}\n")
+
+print_wbw("You encounter some enemies!")
+
+while True:
+    print_wbw("Choose an enemy to battle:")
+    for index, enemy in enumerate(enemies, 1):
+        print_wbw(f"{index}. {enemy.name}")
+
+    try:
+        player_choice = int(input("Enter the number of the enemy to battle: "))
+        if 1 <= player_choice <= len(enemies):
+            enemy_choice = enemies[player_choice - 1]
+            print(f"\nYou have chosen to battle {enemy_choice.name}!")
+            print(f"{enemy_choice.name} - Health: {enemy_choice.health}, Attack: {enemy_choice.attack}")
+            break
+        else:
+            print("That enemy is not on the list!")
+    except ValueError:
+        print("Please enter a number :(")
+
+while player.alive and enemy_choice.alive:
+    player.actions()
+    if enemy_choice.alive:
+        enemy_choice.attack(player)
+
+if player.alive:
+    print_wbw("Congratulations! You defeated the enemy!")
+else:
+    print_wbw("Oh no! The enemy has defeated you. Better luck next time!")
